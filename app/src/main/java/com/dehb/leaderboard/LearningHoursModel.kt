2eb626453ecddd1166eb86c2e2f1f913e08d9ba7
dehb <teejaysunimi@gmail.com>
2020-09-08 15:17:44 +0100

    data model class

---
+++ app/src/main/java/com/dehb/leaderboard/LearningHoursModel.kt
@@ -0,0 +1,8 @@
+package com.dehb.leaderboard
+
+data class LearningHoursModel (
+    val name: String,
+    val hours: String,
+    val country: String,
+    val badgeUrl:String
+)
---
+++ app/src/main/java/com/dehb/leaderboard/SkillIqModel.kt
@@ -0,0 +1,8 @@
+package com.dehb.leaderboard
+
+data class SkillIqModel(
+    val name: String,
+    val score: String,
+    val country: String,
+    val badgeUrl:String
+)
