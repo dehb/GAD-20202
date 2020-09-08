f17d4a56bf2fe32d49b16db2991fba70d4889128
dehb <teejaysunimi@gmail.com>
2020-09-08 14:32:41 +0100

    learning leaders data display with recycler view

--- app/src/main/java/com/dehb/leaderboard/LearningLeaderFragment.kt
+++ app/src/main/java/com/dehb/leaderboard/LearningLeaderFragment.kt
@@ -1,17 +1,30 @@
 package com.dehb.leaderboard
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+//import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
+import com.dehb.leaderboard.ViewModel
+import com.dehb.leaderboard.ui.main.PageViewModel
 import kotlinx.android.synthetic.main.fragment_learing_leaders.*
 import kotlinx.android.synthetic.main.fragment_learing_leaders.view.*
 
 class LearningLeaderFragment : Fragment() {
+    val viewModel: ViewModel by activityViewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        viewModel.getLearningLeader(requireContext())
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,13 +32,24 @@ class LearningLeaderFragment : Fragment() {
         // Inflate the layout for this fragment
         val root = inflater.inflate(R.layout.fragment_learing_leaders, container, false)
 
-        setUpRecycler(root.learning_recycler)
+//        setUpRecycler(root.learning_recycler)
+        viewModel.learningLeaders.observe(viewLifecycleOwner, Observer<List<LearningHoursModel>>{
+           val learningLeaders = it ?: return@Observer
+            Log.i("Response_observer", learningLeaders.toString())
+
+//            if (learningLeaders.isNullOrEmpty()){
+//                timetable_fragment.addView(View.inflate(context, R.layout.empty_response_view, null ))
+//            } else {
+            setUpRecycler(root.learning_recycler, learningLeaders)
+
+//            }
+        })
         return root
     }
 
 
-    private fun setUpRecycler(view: RecyclerView) = with(view){
+    private fun setUpRecycler(view: RecyclerView, learningData: List<LearningHoursModel>) = with(view){
         layoutManager = LinearLayoutManager(context)
-        adapter = LearningRecyclerAdapter()
+        adapter = LearningRecyclerAdapter(learningData, R.layout.learning_list_item)
     }
 }
--- app/src/main/java/com/dehb/leaderboard/LearningRecyclerAdapter.kt
+++ app/src/main/java/com/dehb/leaderboard/LearningRecyclerAdapter.kt
@@ -3,24 +3,34 @@ package com.dehb.leaderboard
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.learning_list_item.view.*
 
-class LearningRecyclerAdapter(): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+class LearningRecyclerAdapter(private val learningLeaders: List<LearningHoursModel>, private val listLayout: Int): RecyclerView.Adapter<LearningRecyclerAdapter.ViewHolder>(){
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
+        val personBadge = view.badge
+        val personName= view.name
+        val learnHours = view.hours
+        val country = view.country
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.learning_list_item, parent, false)
+            LayoutInflater.from(parent.context).inflate(listLayout, parent, false)
         )
 
     }
 
-    override fun getItemCount(): Int =15
+    override fun getItemCount(): Int {
+    return learningLeaders.size
+    }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//        holder.personBadge =learningLeaders[position].badgeUrl
+        holder.personName.text = learningLeaders[position].name
+        holder.learnHours.text =learningLeaders[position].hours
+        holder.country.text =learningLeaders[position].country
     }
 }
